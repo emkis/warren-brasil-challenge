ea8c8b017d2926c4c2336325da04048de238e434
emkis <nicolasemkis@gmail.com>
2020-11-24 21:56:56 -0300

    chore(eslint): ignore experimental rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   rules: {
     semi: ['error', 'never'],
     'prettier/prettier': 'error',
+    'vue/experimental-script-setup-vars': 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },

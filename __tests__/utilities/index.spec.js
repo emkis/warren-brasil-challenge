c4accfe6f9452e8ffebaa322426e26f2691cf236
emkis <nicolasemkis@gmail.com>
2020-11-25 01:55:31 -0300

    feat: add BRL currency formatter

--- __tests__/utilities/index.spec.js
+++ __tests__/utilities/index.spec.js
@@ -1,17 +1,37 @@
-import { removeAccent } from '@/utilities'
+import { removeAccent, formatCurrencyBRL } from '@/utilities'
 
 describe('Utility Functions', () => {
   describe('removeAccent()', () => {
-    it('Should return a string without accents', () => {
+    it('should return a string without accents', () => {
       const targetString = 'Héllõ Wârrèn'
       const normalizedString = removeAccent(targetString)
 
       expect(normalizedString).toBe('Hello Warren')
     })
 
-    it('Should return an empty string when nothing is provided', () => {
+    it('should return an empty string when nothing is provided', () => {
       const normalizedString = removeAccent()
       expect(normalizedString).toBe('')
+    })
+  })
+
+  describe('formatCurrencyBRL()', () => {
+    it('should return a formatted string with BRL currency', () => {
+      const value = 1560
+      const formattedValue = formatCurrencyBRL(value)
+
+      expect(formattedValue).toBe('R$\xa01,560.00')
+    })
+
+    it('should return a value formatted zero value when nothing is provided', () => {
+      expect(formatCurrencyBRL()).toBe('R$\xa00.00')
+    })
+
+    it('should return a formatted BRL currency without prefix when provided false in the second argument', () => {
+      const value = 2423.54
+      const formattedValue = formatCurrencyBRL(value, false)
+
+      expect(formattedValue).toBe('2,423.54')
     })
   })
 })
--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,3 +1,12 @@
 export function removeAccent(targetString = '') {
   return targetString.normalize('NFD').replace(/[\u0300-\u036f]/g, '')
 }
+
+export function formatCurrencyBRL(value = 0, usePrefix = true) {
+  const withPrefixOptions = { style: 'currency', currency: 'BRL' }
+  const withoutPrefixOptions = { minimumFractionDigits: 2 }
+
+  const localeOptions = usePrefix ? withPrefixOptions : withoutPrefixOptions
+
+  return value.toLocaleString('pt-br', localeOptions)
+}

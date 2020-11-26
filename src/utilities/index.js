da2fde32e3306932a497c1e339ea59983b94b6ae
emkis <nicolasemkis@gmail.com>
2020-11-26 10:31:24 -0300

    feat: add utility functions

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -10,3 +10,28 @@ export function formatCurrencyBRL(value = 0, usePrefix = true) {
 
   return value.toLocaleString('pt-br', localeOptions)
 }
+
+export function groupArrayByProp(propName, targetArray) {
+  return targetArray.reduce((acc, currentValue) => {
+    const currentProp = currentValue[propName]
+
+    acc[currentProp] = [...(acc[currentProp] || []), currentValue]
+    return acc
+  }, {})
+}
+
+export function formatDateString(targetDate) {
+  const [year, month, day] = targetDate.split('-')
+  const parsedDate = new Date(year, month - 1, day)
+  const localeOptions = { day: 'numeric', month: 'long', year: 'numeric' }
+
+  return parsedDate.toLocaleDateString('pt-BR', localeOptions)
+}
+
+export function toggleAppScroll() {
+  const appStyles = document.body.style
+  const isScrollPrevented = appStyles.overflowY === 'hidden'
+
+  if (isScrollPrevented) appStyles.overflowY = ''
+  else appStyles.overflowY = 'hidden'
+}

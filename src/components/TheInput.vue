d6aee6c8143c73e71564852ae3a86426e1ab1913
emkis <nicolasemkis@gmail.com>
2020-11-26 10:49:59 -0300

    feat(component): add basic input

---
+++ src/components/TheInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <fieldset class="TheInput">
+    <label class="sr-only">{{ label }}</label>
+
+    <input
+      type="text"
+      v-bind="$attrs"
+      v-on="$listeners"
+      class="TheInput__field"
+      :value="value"
+    />
+  </fieldset>
+</template>
+
+<script>
+export default {
+  name: 'TheInput',
+  inheritAttrs: false,
+  props: {
+    label: { type: String, required: true },
+    value: { type: String, required: true },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.TheInput {
+  border: 0;
+  padding: 0;
+
+  &__field {
+    padding: 8px 16px;
+  }
+}
+</style>

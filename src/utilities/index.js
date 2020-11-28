11ad9df814609c6f7a548f93cc7a3e728ff3b160
emkis <nicolasemkis@gmail.com>
2020-11-27 23:42:37 -0300

    fix: prevent page jump when toggle scroll

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -31,9 +31,15 @@ export function formatDateString(targetDate) {
 export function toggleAppScroll() {
   const appStyles = document.body.style
   const isScrollPrevented = appStyles.overflowY === 'hidden'
+  const navbarSize = '15px'
 
-  if (isScrollPrevented) appStyles.overflowY = ''
-  else appStyles.overflowY = 'hidden'
+  if (isScrollPrevented) {
+    appStyles.overflowY = ''
+    appStyles.marginRight = ''
+  } else {
+    appStyles.overflowY = 'hidden'
+    appStyles.marginRight = navbarSize
+  }
 }
 
 export function asyncDelay(ms = 1000) {

9cc62ba40555baa6be06ea1150cdf2f4ffa72cff
emkis <nicolasemkis@gmail.com>
2020-11-28 18:36:22 -0300

    test: add AppInput component test

---
+++ __tests__/components/AppInput.spec.js
@@ -0,0 +1,31 @@
+import { shallowMount } from '@vue/test-utils'
+import AppInput from '@/components/AppInput'
+
+describe('AppInput', () => {
+  it('should render provided props correctly', () => {
+    const wrapper = shallowMount(AppInput, {
+      propsData: {
+        label: 'A really nice label',
+        value: 'Lorem ipsum sit dolor',
+      },
+    })
+
+    const Label = wrapper.find('label')
+    const Input = wrapper.find('input')
+
+    expect(Label.text()).toBe('A really nice label')
+    expect(Input.element.value).toBe('Lorem ipsum sit dolor')
+  })
+
+  it('should set default type when "type" prop not provided', () => {
+    const wrapper = shallowMount(AppInput, {
+      propsData: {
+        label: 'A really nice label',
+        value: 'Lorem ipsum sit dolor',
+      },
+    })
+
+    const Input = wrapper.find('input')
+    expect(Input.element.type).toBe('text')
+  })
+})

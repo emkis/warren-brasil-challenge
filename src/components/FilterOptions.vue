940fc0414b20c0fcff277289bff9aab00e0f1f2d
emkis <nicolasemkis@gmail.com>
2020-11-28 04:30:19 -0300

    feat(component): add filter options

---
+++ src/components/FilterOptions.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="FilterOptions">
+    <button
+      :key="option.value"
+      v-for="option in options"
+      class="FilterOptions__option"
+      :class="{
+        'FilterOptions__option--active': option.value === activeOption,
+      }"
+      @click="setActiveOption(option)"
+    >
+      {{ option.name }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FilterOptions',
+  props: {
+    options: { type: Array, required: true },
+    value: { type: String, required: false },
+  },
+  data() {
+    return {
+      activeOption: this.value,
+    }
+  },
+  computed: {
+    uuid() {
+      return Math.random().toString(16).slice(10)
+    },
+  },
+  methods: {
+    setActiveOption(option) {
+      this.activeOption = option.value
+      this.$emit('on-select', option.value)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.FilterOptions {
+  display: flex;
+  align-items: center;
+
+  &__option {
+    padding: 6px 20px;
+    font-size: 14px;
+    font-family: $font-title;
+    font-weight: 500;
+    line-height: 1.5;
+    color: $color-gray-text;
+    border-radius: 50px;
+    border: 1px solid transparent;
+    background: $color-gray-element;
+    cursor: pointer;
+
+    &--active {
+      color: $color-warren;
+      background: rgba($color-warren, 0.2);
+    }
+
+    & + & {
+      margin-left: 10px;
+    }
+  }
+}
+</style>

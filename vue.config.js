d3a6fdb90dda41fdb973c3534eaa5ff9540dd1c3
emkis <nicolasemkis@gmail.com>
2020-11-29 20:14:11 -0300

    chore(deps): add vue svg loader

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,11 @@ module.exports = {
       title: process.env.VUE_APP_TITLE || 'Warren Brasil Challenge',
     },
   },
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule.use('vue-svg-loader').loader('vue-svg-loader')
+  },
   css: {
     loaderOptions: {
       sass: {

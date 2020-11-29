d8e4b976ebc9b6d3a7c47f5aa089bad7aa2eebf0
emkis <nicolasemkis@gmail.com>
2020-11-29 02:17:09 -0300

    feat(component): add icon close

---
+++ src/components/icons/IconClose.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    :height="size"
+    :width="size"
+    viewBox="0 0 17 17"
+    fill="none"
+    :stroke="color"
+    xmlns="http://www.w3.org/2000/svg"
+    role="presentation"
+    stroke-width="2"
+  >
+    <path d="M1 1L16 16" />
+    <path d="M1 16L16 1" />
+  </svg>
+</template>
+
+<script>
+import { iconMixin } from '@/mixins/iconMixin'
+
+export default {
+  name: 'IconClose',
+  ...iconMixin,
+}
+</script>

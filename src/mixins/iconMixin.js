9e215d13e75cc8531c988747f89f82e07afa6ae3
emkis <nicolasemkis@gmail.com>
2020-11-29 02:16:52 -0300

    feat: add icon mixin

---
+++ src/mixins/iconMixin.js
@@ -0,0 +1,12 @@
+export const iconMixin = {
+  props: {
+    color: {
+      type: String,
+      default: 'currentColor',
+    },
+    size: {
+      type: [String, Number],
+      default: '16',
+    },
+  },
+}

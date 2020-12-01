2b55925ddd83e9ce5a52498596c34be8d7d3621c
emkis <nicolasemkis@gmail.com>
2020-11-30 23:56:00 -0300

    feat: add theme handler to main file

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
+
 import App from './App.vue'
+import { initTheme } from '@/theme'
 
 import '@/styles/globals.scss'
+import '@/styles/themes/index.scss'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: (h) => h(App),
+  mounted() {
+    initTheme()
+  },
 }).$mount('#app')

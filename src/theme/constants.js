63edabe324ec014b553810bd876f90fb83a1d30d
emkis <nicolasemkis@gmail.com>
2020-11-30 23:54:56 -0300

    feat: add theme handlers & constants

---
+++ src/theme/constants.js
@@ -0,0 +1,7 @@
+export const THEME_STORAGE_KEY = 'theme-mode'
+export const THEME_APP_ATTRIBUTE = 'data-theme'
+
+export const themesEnum = Object.freeze({
+  LIGHT: 'light',
+  DARK: 'dark',
+})
---
+++ src/theme/functions.js
@@ -0,0 +1,47 @@
+import { THEME_APP_ATTRIBUTE, THEME_STORAGE_KEY, themesEnum } from '@/theme'
+
+export function isThemeValid(themeName) {
+  const themes = Object.values(themesEnum)
+  return themes.includes(themeName)
+}
+
+export function setTheme(themeName) {
+  if (!isThemeValid(themeName)) return
+
+  const app = document.querySelector('#app')
+  app.setAttribute(THEME_APP_ATTRIBUTE, themeName)
+  storeTheme(themeName)
+}
+
+export function storeTheme(themeName) {
+  localStorage.setItem(THEME_STORAGE_KEY, themeName)
+}
+
+export function getStoredTheme() {
+  return localStorage.getItem(THEME_STORAGE_KEY)
+}
+
+export function isUserSystemDarkThemeActive() {
+  return window.matchMedia('prefers-color-scheme: dark').matches
+}
+
+export function initTheme() {
+  const theme = getStoredTheme()
+  const isThemeAlreadyStored = Boolean(theme)
+
+  if (isThemeAlreadyStored) {
+    setTheme(theme)
+    return
+  }
+
+  if (isUserSystemDarkThemeActive()) {
+    setTheme(themesEnum.DARK)
+    return
+  }
+
+  setTheme(themesEnum.LIGHT)
+}
+
+export function getCurrentTheme() {
+  return getStoredTheme() || themesEnum.LIGHT
+}

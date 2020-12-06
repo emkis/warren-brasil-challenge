531c621f84ff898362c430db481730122084b761
emkis <nicolasemkis@gmail.com>
2020-12-06 05:45:11 -0300

    feat: add lazy import transaction components

---
+++ src/components/TransactionHistory/lazyImports.js
@@ -0,0 +1,11 @@
+export const TransactionHistory = () => {
+  return import(
+    /* webpackChunkName: "TransactionHistory" */ './TransactionHistory'
+  )
+}
+
+export const TransactionHistoryError = () => {
+  return import(
+    /* webpackChunkName: "TransactionHistoryError" */ './TransactionHistoryError'
+  )
+}
